98f737974b76a65cb5cddcfd77713562b973d738
Atul <atppatil18@gmail.com>
2021-07-18 10:23:28 +0530

    Initial Commit

---
+++ Shared/Poonam_SofasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poonam_SofasApp.swift
+//  Shared
+//
+//  Created by Atul Patil on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Poonam_SofasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
